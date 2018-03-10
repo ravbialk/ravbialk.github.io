671f6e90054fc5cbfa6546b3c3c40f8448494600
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-10 16:57:24 +0100

    Funkcje

---
+++ warsztaty/javascript/js/funkcje.js
@@ -0,0 +1,26 @@
+// funkcja bez parametrów
+function wyswietlWKonsoli () {
+    console.log( "Adam Nowacki");
+}
+
+wyswietlWKonsoli ();
+
+// funcja z parametrami
+
+function wyswietlWKonsoliParam(imie, nazwisko){
+    console.log( imie +" " + nazwisko );
+}
+wyswietlWKonsoliParam ("Adam", "Nowacki");
+wyswietlWKonsoliParam ("Katarzyna", "Izak");
+wyswietlWKonsoliParam ("Kinga", "Korycka");
+wyswietlWKonsoliParam ("Adrian", "Bak");
+
+//zwracanie wartości przez funkcje
+
+function obliczPodatek( kwota, procentPodatek){
+    var wynik = kwota * (procentPodatek/100);
+    return wynik.toFixed(2);
+}
+
+var obliczenia1 = obliczPodatek (4990, 23);
+console.log ("Wysokość obliczonego podatku wynosi: " + obliczenia1);
