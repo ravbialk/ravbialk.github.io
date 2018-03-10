7583b45406aa4740801f04730995fba10d47e274
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-10 15:51:02 +0100

    zmienne

---
+++ warsztaty/javascript/js/zmienne.js
@@ -0,0 +1,12 @@
+//console.log ("Tadzio Wiertełko");
+
+
+var imie;
+var nazwisko = "Nowacki";
+var wiek = 32;
+
+// łączenie zmiennych
+
+var laczenieTekstu = "Moje imie to: " + imie + " a nazwisko to: " + nazwisko;
+
+console.log( laczenieTekstu );
