b01635f131b39d55262531621a92093efe1f6c6f
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-11 14:01:27 +0100

    Math

---
+++ warsztaty/javascript/js/math.js
@@ -0,0 +1,11 @@
+// właściwości obiektu Match
+
+console.log (Math.E);
+console.log (Math.PI);
+
+//metody obiektu Match
+
+console.log(Math.cos(30));
+console.log(Math.random());
+console.log(Math.pow(2,2));
+
