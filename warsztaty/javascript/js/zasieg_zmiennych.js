962e4d6fe53b5ad7f3bda985ba3f28bba235f3e1
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-11 09:48:38 +0100

    Zmienne globalne i lokalne

---
+++ warsztaty/javascript/js/zasieg_zmiennych.js
@@ -0,0 +1,24 @@
+/*var zmiennaGlobalna ="Tak! Jestem zmienną globalną i jestem widoczna w funkcjach";
+
+function wyswietlZmienna () {
+    console.log(zmiennaGlobalna);
+    var zmiennaLokalna = "Tak! Zmienna lokalna to ja. Przestrzeńglobalna mnie nie zobaczy";
+}
+wyswietlZmienna();
+console.log(zmiennaLokalna);*/
+//
+//
+
+
+var zmienna = "Zmienna globalna";
+
+function pokazZmienna (){
+    
+    var zmienna = "Zmienna lokalna";
+    console.log(zmienna);
+}
+
+// pokaz zmienna();
+
+console.log(zmienna);
+
