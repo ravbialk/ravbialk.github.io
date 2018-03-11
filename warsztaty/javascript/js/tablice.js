d612ad9722938534a25fd3939504d754ad52494b
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-11 11:06:59 +0100

    Tablice

---
+++ warsztaty/javascript/js/tablice.js
@@ -0,0 +1,38 @@
+// Tablice
+
+var imiona = ['Adam', 'Marta', 'Paweł', 'Michał', 'Kasia', 'Sandra'];
+
+// dowawanie elementów na końcu tablicy
+imiona[6]= 'Mikołaj';
+imiona[7]= 'Kinga';
+
+imiona.push("Kamil", "Krzysztof", "Anna");
+imiona.push("Marcin")
+
+//usuwanie elementów z końca tablicy
+imiona.pop();
+
+//dodawanie elementów tablicy na początku
+
+imiona.unshift("Sebastian");
+imiona.unshift("Małgosia");
+
+// usuwanie elementów z początku tablicy (pierwszy element)
+
+imiona.shift();
+
+
+console.log(imiona);
+
+// sprawdzenie "długości" tablicy
+
+var iloscElementówTablicy = imiona.length;
+console.log(iloscElementówTablicy);
+
+// robijanie tablicy na ciąg tekstowy - metoda join
+
+
+//var elementyTablicyString = imiona.join();
+var elementyTablicyString = imiona.join(", ");
+
+console.log(elementyTablicyString);
