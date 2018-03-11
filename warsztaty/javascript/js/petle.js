d612330c894c421989491f2d5f5ee55eb4aad1ee
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-11 15:51:49 +0100

    Petla_1

---
+++ warsztaty/javascript/js/petle.js
@@ -0,0 +1,27 @@
+// Pętle //Iteracja - jeden obrót pętli
+
+//for( var i = 1; i<=10; i++){
+//    console.log("Iteracja nr " + i);  
+//}
+
+
+// length - liczy ilości warunków z tablicy
+
+var tablica = ["Volvo", "Mazda", "Honda", "Audi"];
+//var dlugoscTablicy = tablica.length;
+//for (var i=0;i<dlugoscTablicy; i++){
+ //   console.log(tablica[i]);
+//}
+
+// pętla forEach
+
+//tablica.forEach (function(element, index) {
+//                 console.log("Index: " + index + "Element: " + element);
+//                 } );
+
+
+var iter= 0;
+while (iter < 25) {
+    console.log ("While iteracja nr " + iter);
+    iter ++;
+}
