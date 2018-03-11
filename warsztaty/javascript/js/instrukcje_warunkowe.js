42da042a6e67fd1d7a547b8f5796ffc6d3ae2f59
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-11 15:21:47 +0100

    Switch

---
+++ warsztaty/javascript/js/instrukcje_warunkowe.js
@@ -0,0 +1,48 @@
+// Instrukcja if
+
+var zmienna1 = "Kot";
+var zmienna2= "Pies";
+
+//if(zmienna1 != zmienna2){
+//    console.log("Spełaniam warunek");
+//}
+
+
+// instrukcja if else
+
+//if (zmienna1 == zmienna2) {
+ //   console.log("Spełniam warunek");
+//} else {
+ //   console.log("Nie spełaniam warunku");
+//}
+
+// instrukcja else if
+
+/*var mojaLiczba =111;
+
+if(mojaLiczba == 5) {
+    console.log("Moja liczba to 5");
+}else if(mojaLiczba == 12) {
+     console.log("Moja liczba to 12");
+}else if(mojaLiczba == 65) {
+     console.log("Moja liczba to 65");
+    }else if(mojaLiczba == 99) {
+     console.log("Moja liczba to 99");
+        else {
+             console.log("Moja liczba ma inna wartość");
+        }*/
+
+// Instrukcja SWITCH
+        
+        
+var kolor = "zielony";
+        switch (kolor){
+            case "czerwony" :
+                console.log("kolor czerwony");
+                break;
+                  case "zielony" :
+                console.log("kolor zielony");
+                break;
+                default :
+                console.log("Nie znalazłem dopasowania");
+        }
