9b36b6ffa0e391746be16f4bdcdf0232dc05d060
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-16 14:17:54 +0100

    Sortowanie_babelkowe_ok

--- zadania-domowe/Modul_3/Zadania_js/8_sortowanie_babelkowe.js
+++ zadania-domowe/Modul_3/Zadania_js/8_sortowanie_babelkowe.js
@@ -1,29 +1,47 @@
+//
+//var tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+//
+//console.log(tablica);
+//
+//function sortowanieBabelkowe(){
+//  
+//  var n=tablica.length;
+//  do 
+//{
+//for(var i=0; i<n-1; i++)
+//  {
+//  if ( tablica[i]>tablica[i+1])
+//    {
+//  var babel=tablica[i];
+//  tablica[i]=tablica[i+1];
+//  tablica[i+1]=babel;
+//    }
+//  }
+//  n=n-1;
+//} while (n>1)
+//
+//}
+//
+//console.log(sortowanieBabelkowe());
+//console.log(tablica);
+    
+var tablica = [  12,  67,  34,  23.01,  24,  2,  56,  8,  0x10,  23.02,  78,  34,  1e2,  45,  67,  98,  1,];
 
-var tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
-
-console.log(tablica);
-
-function sortowanieBabelkowe(){
-  
-  var n=tablica.length;
-  do 
-{
-for(var i=0; i<n-1; i++)
-  {
-  if ( tablica[i]>tablica[i+1])
-    {
-  var babel=tablica[i];
-  tablica[i]=tablica[i+1];
-  tablica[i+1]=babel;
+function sortowanie(tablica) {
+  var zmiana;
+  var licznik;
+  do {
+    zmiana = false;
+    for (var i = 0; i < tablica.length - 1; i++) {
+      if (tablica[i + 1] < tablica[i]) {
+        licznik = tablica[i];
+        tablica[i] = tablica[i + 1];
+        tablica[i + 1] = licznik;
+        zmiana = true;
+      }
     }
-  }
-  n=n-1;
-} while (n>1)
-
+  } while (zmiana);
+  return tablica;
 }
 
-console.log(sortowanieBabelkowe());
-console.log(tablica);
-    
-    
-
+sortowanie(tablica);
