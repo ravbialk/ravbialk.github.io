d943ed23c9615b04a54751b808b0c37abeec8ea5
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-17 10:27:21 +0100

    Dom-obiekty

---
+++ warsztaty/dom/js/model_dom.js
@@ -0,0 +1,19 @@
+//console.log(document);
+//console.dir(document);
+//console.dir(document.baseURI);
+
+// zwróci element body jako obiekt
+//console.dir(document.body);
+
+// zwróci element html jako obiekt
+
+//console.log(document.documentElement);
+
+
+// obiekt window
+
+//console.log(window);
+
+console.log (window.innerWidth);
+console.log (window.innerHeight);
+console.log (window.document);
