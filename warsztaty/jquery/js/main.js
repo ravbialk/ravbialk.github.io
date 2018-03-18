4e0591f5a9b36ad9a83b58801e460790b920e967
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-18 11:37:06 +0100

    Jquery_document.ready

---
+++ warsztaty/jquery/js/main.js
@@ -0,0 +1,9 @@
+// document.ready gwarantuje, że skrypty wywołają się w momencie gdy cały model DOM jest juz załadowany
+// poniżej skrócony zapis document.ready
+$(function(){
+    
+//    $(".title").css({color: "red", backgroundColor:"yellow"}); //I wersja
+//    $("h2").css({color: "red", backgroundColor:"yellow"}); // II wersja
+    $("[class='title']").css({color: "red", backgroundColor:"yellow"}); // III wersja - po klasie
+    
+});
