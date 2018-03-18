4480e3d2eec16934f62f2a1eecbecc3076c8ed77
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-18 14:59:49 +0100

    Jquery - aniamacje

---
+++ warsztaty/jquery/js/animacje.js
@@ -0,0 +1,16 @@
+var btn = $("#animate");
+
+btn.on("click", function(){
+//      $("h1").hide(4000).show("fast");
+//      $("h1").fadeOut(3000).fadeIn("slow");
+//      $("h1").slideUp(3000).slideDown(400);
+    
+    $("h1").animate(
+    {"left":"100px", "top": "100px"},
+    4000,
+    function() {
+        alert("Koniec animacji");
+    }   
+    );
+    
+});
