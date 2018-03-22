2172864b23d2959afa93187b536920bcc5a587b9
Rafał Białkowski <rav_bialk@o2.pl>
2018-03-22 19:19:06 +0100

    Zadania_domowe_kalkulator

--- zadania-domowe/Modul_4/Zadanie-1/kalkulator.js
+++ zadania-domowe/Modul_4/Zadanie-1/kalkulator.js
@@ -1,36 +1,14 @@
-// I sposób
-function wyswietl (znak) {
-  document.getElementById("ekran").value += znak
-  
+
+
+function wynikDzialania(){
+  var x = document.getElementById('ekran').value;
+  document.getElementById('ekran').value = eval(x);
 }
 
-function wynik() {
-  var ekran = document.getElementById("ekran").value;
-  document.getElementById("ekran").value = eval(ekran);
+function pokazZnak(x) {
+  document.getElementById('ekran').value += x;
 }
 
 function wyczysc() {
-document.getElementById("ekran").value = " ";}
-
-
-//II sposób
-
-var button = document.getElementById('button');
-var cyfry = document.getElementsByClassName('cyfra');
-var operacje = document.getElementsByClassName('opercaja');
-var wynik = document.getElementsByClassName('rownasie');
-var wyczysc = document.getElementsByClassName('wyczysc');
-var ekran = document.getElementById('ekran');
-
-
-function addValue(num) {
-	ekran.value += num;
-	
-	if (num == wyczysc) {
-	  ekran.value === '';
-	}
-	
-	if( num == wynik) {
-	  ekran.value == eval(ekran.value);
-	}
+  document.getElementById("ekran").value = ' ';
 }
